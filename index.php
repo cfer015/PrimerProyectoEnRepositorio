361abfa83ffa2f7f3a7ec44139d30cc87afa2c56
Cristhian <cristhianwf@gmail.com>
2012-08-03 16:30:30 -0500

    Mejor esta
    adfasdf

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 	<title>Proyecto Con Repositorio</title>
 </head>
 <body>
-	<h3>Holas a todos como van</h3>
+	<h3>Holas a todos como van la otra parte seria todas configuraciones que se puedan hacer aki normalmente aceptamos todo lo que se viene terminando las cosas en comparacion con las demas cosas en comun</h3>
+	<div>
+		Este es mi primer proyecto en joomla normalmente estaria jodido pero ahi vamos jodiendome la vida
+	</div>
 </body>
 </html>
