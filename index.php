c2ecfba59344f876e7c193a23062ae34ad78b69f
Cristhian <cristhianwf@gmail.com>
2012-08-03 18:11:19 -0500

    asdfasdf
    adsfasdf

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<div>wao tamos normalmente XD</div>
 	<p>
 		terminal de datos estara ajki joder joder ptmadsf
+		adsf adsfadsf
+		asdf asdf  terminal de datos normalmente terminamos algunas cosas normalemnte terminamos los datos jejej aki estamos bien vamos a 
 	</p>
 </body>
 </html>
