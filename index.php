f732f861f7c9366dda74cfb282b7400cc956f35f
Cristhian <cristhianwf@gmail.com>
2012-08-03 19:36:50 -0500

    numero1
    asdf

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 		adsf adsfadsf
 		asdf asdf  terminal de datos normalmente terminamos algunas cosas normalemnte terminamos los datos jejej aki estamos bien vamos a asdfad
 		asdf asdf asdfadsf ++++++++++++++++++++++++++++++++++
+		normalmente yo aria esto pero como no se puede asi numas		
 	</p>
 </body>
 </html>
