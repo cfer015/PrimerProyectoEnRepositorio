4a8fa0ba36166345b3e26b576401fa8bb2d0a9d5
Cristhian <cristhianwf@gmail.com>
2012-08-03 19:54:59 -0500

    Mi Hola Mundo XD
    jejeje

--- index.php
+++ index.php
@@ -6,21 +6,7 @@
 	<h3>Holas a todos como van</h3>
 	<div>wao tamos normalmente XD</div>
 	<p>
-		terminal de datos estara ajki joder joder ptmadsf
-		adsf adsfadsf
-		asdf asdf  terminal de datos normalmente terminamos algunas cosas normalemnte terminamos los datos jejej aki estamos bien vamos a asdfad
-		asdf asdf asdfadsf ++++++++++++++++++++++++++++++++++
-		normalmente yo aria esto pero como no se puede asi numas
-		adsf asdf
-		adsfaf
-		adsfads adsf
-		a
-		adsf asf
-		asdf
-		adsf asdf
-		adsf
-		adsfadsf
-		asdfadsf asdfasdf		
+		hola mundo		
 	</p>
 </body>
 </html>
